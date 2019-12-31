b060a72e818fa0e7674d0286d407f0522f34649b
Aktar Zaman <aktarzaman@berkeley.edu>
2019-12-31 00:32:44 -0800

    working on the new app

--- algorithms_explained/algo_explained/apps.py
+++ algorithms_explained/algo_explained/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 
 class AlgoExplainedConfig(AppConfig):
     name = 'algo_explained'
+
+    verbose_name = "Explain_Algo"
+
+    def ready(self):
+        try:
+            # noinspection PyUnresolvedReferences
+            from . import signals  # noqa F401
+        except ImportError:
+            pass
