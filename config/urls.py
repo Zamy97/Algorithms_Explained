bd1e43e264fe5790fa3ee298f1306f5fe5d84691
Aktar Zaman <aktarzaman@berkeley.edu>
2020-01-02 18:55:11 -0800

    trying to deploy on heroku

--- config/urls.py
+++ config/urls.py
@@ -6,10 +6,10 @@ from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
 urlpatterns = [
-    # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    # path(
-        # "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    # ),
+    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path(
+        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
+    ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
