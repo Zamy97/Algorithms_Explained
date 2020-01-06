9212d4934edc8dcb4f795261d6601c3ddc03fc2b
Aktar Zaman <aktarzaman@berkeley.edu>
2020-01-05 20:00:14 -0800

    added a way to see the post and category title in the admin site

--- algorithms_explained/algo_explained/models.py
+++ algorithms_explained/algo_explained/models.py
@@ -5,12 +5,18 @@ from django.db.models import CharField
 class Category(models.Model):
     name            = models.CharField(max_length = 20)
 
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title           = models.CharField(max_length = 255)
     body            = models.TextField()
     created_on      = models.DateTimeField(auto_now_add = True)
     last_modified   = models.DateTimeField(auto_now = True)
     categories      = models.ManyToManyField('Category', related_name = "posts")
+
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     author          = models.CharField(max_length = 60)
